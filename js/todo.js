53d75e662544e3967a5bda33bc065abf0a2bcdb8
Vishal R <vishivish18@gmail.com>
2015-07-05 21:09:36 +0530

    minor tweaks

--- js/todo.js
+++ js/todo.js
@@ -2,8 +2,7 @@ angular.module('todoApp', [])
   .controller('TodoListController', function() {
     var todoList = this;
     todoList.todos = [
-      {text:'learn angular', done:true},
-      {text:'build an angular app', done:false}];
+    ];
  
     todoList.addTodo = function() {
       todoList.todos.push({text:todoList.todoText, done:false});
