9736c59728a1cf72cb52ca41a14dfc3074d8e0f6
Vishal R <vishivish18@gmail.com>
2015-07-18 22:23:40 +0530

    test commit for controller

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,5 +2,16 @@ app.controller('MainController', ['$scope', function($scope){
 	
 	$scope.name = "vishal";
 	$scope.age = 24;
+	$scope.details = [
+	{
+		title : "ranjan",
+		location : "Delhi"
+
+
+	}
+	
+	];
+
+
 
 }])
